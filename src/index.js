df93074d739bd41730a1a7dd726e9f415f4add40
Honorio <htaveras90@gmail.com>
2021-02-19 18:16:40 -0700

    set up babel and webpack with react, rendering app component, hotload for changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('sup dirtbag!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './Components/App/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+module.hot.accept();
