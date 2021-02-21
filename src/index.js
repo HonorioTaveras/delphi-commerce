8611e23cb191f2018b545142c2106611b55304eb
Honorio <htaveras90@gmail.com>
2021-02-21 15:41:11 -0700

    redesigned folder structure for context api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './Components/App/App';
+import App from './App/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
