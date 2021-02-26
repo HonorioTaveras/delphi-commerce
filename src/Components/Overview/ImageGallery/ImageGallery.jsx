9d46af2a424dbc486754250462a7875ec2778390
Honorio <htaveras90@gmail.com>
2021-02-25 22:55:51 -0700

    imported bootstrap for the carousel. rendering photos for the current style in image gallery component

--- src/Components/Overview/ImageGallery/ImageGallery.jsx
+++ src/Components/Overview/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+import React, { useContext, Children } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
 import { FetchContext } from '../../../providers/FetchProvider';
 
@@ -6,10 +7,22 @@ import './ImageGallery.scss';
 
 const ImageGallery = () => {
   const { productStyles, currentStyleIdx } = useContext(FetchContext);
+  const currentStyle = productStyles[currentStyleIdx];
 
-  console.log(productStyles[currentStyleIdx]);
-
-  return <div>Sup dirtbag! from ImageGallery component</div>;
+  return (
+    <div>
+      <Carousel>
+        {currentStyle &&
+          Children.toArray(
+            currentStyle.photos.map(({ url }) => (
+              <Carousel.Item>
+                <img src={url} alt='' />
+              </Carousel.Item>
+            ))
+          )}
+      </Carousel>
+    </div>
+  );
 };
 
 export default ImageGallery;
