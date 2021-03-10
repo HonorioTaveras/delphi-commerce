224ea3c9a72f0002c9b8c1c677227c36f33711ff
Honorio <htaveras90@gmail.com>
2021-03-09 22:55:15 -0700

    rendering without style all of the information from product information

--- src/Components/Overview/ProductInformation/ProductInformation.jsx
+++ src/Components/Overview/ProductInformation/ProductInformation.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { OverviewContext } from '../../../providers/overview/OverviewProvider';
 
 import './ProductInformation.scss';
 
-const ProductInformation = () => (
-  <div className="product-information-container">
-    <div className="product-category">Category</div>
-    <div className="product-name">Name</div>
-    <div className="product-price">Price</div>
-  </div>
-);
+const ProductInformation = () => {
+  const { productInformation: { category, name }, productStyles, currentStyleIdx } = useContext(
+    OverviewContext,
+  );
+
+  const currentStyle = productStyles[currentStyleIdx];
+
+  return (
+    <div className="product-information-container">
+      <div className="product-category">{category}</div>
+      <div className="product-name">{name}</div>
+      {currentStyle && currentStyle.sale_price === '0' ? (
+        <div className="display-price">
+          {`$${currentStyle && currentStyle.original_price}`}
+        </div>
+      ) : (
+        <div>
+          <span className="sale-price-block display-price">
+            {`$${currentStyle && currentStyle.sale_price}`}
+          </span>
+          {' '}
+          <span className="sale-price-block struck-price">
+            {`$${currentStyle && currentStyle.original_price}`}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default ProductInformation;
