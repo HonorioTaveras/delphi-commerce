006da936ed6a21ab74898f55ecd2d9a3a15e2772
Honorio <htaveras90@gmail.com>
2021-03-11 15:50:59 -0700

    adding a portl to modal to attach to the div i created in the html

--- src/Components/Overview/ModalGallery/ModalGallery.jsx
+++ src/Components/Overview/ModalGallery/ModalGallery.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 
-const Modal = ({ childen }) => {
-  return (
-    <div className="modal">{ childen }</div>
-  )
-};
+const modalElement = document.getElementById('modal-root');
+
+const Modal = ({ childen }) => createPortal(
+  <div className="modal">{ childen }</div>,
+  modalElement,
+);
 
 export default Modal;
